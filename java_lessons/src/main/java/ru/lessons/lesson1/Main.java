610203897808b08aea839d86d0eeff78eb09e71a
yar83men <yarik83.evpat@gmail.com>
2026-01-28 19:44:17 +0300

    1. Занятие по Java

---
+++ java_lessons/src/main/java/ru/lessons/lesson1/Main.java
@@ -0,0 +1,18 @@
+package ru.lessons.lesson1;
+
+public class Main {
+    public static void main(String[] args) {
+        // Примитивные типы данных
+        byte byteVar = 1;
+        short shortVar = 2;
+        int intVar = 3;
+        long longVar = 4;
+        float floatVar = 500.0f;
+        double doubleVar = 500.0;
+        char charVar = 'a';
+        // Мат операции + - / % *
+        boolean boolVar = true;
+        System.out.println("byteVar * shortVar = " +  byteVar * shortVar);
+
+    }
+}
